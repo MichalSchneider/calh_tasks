232a0644ecc0dbbc57e42eb639773b99ad29f9fe
MichalSchneider <michasmetalik@gmail.com>
2022-07-10 09:44:04 +0200

    wykonano zadanie 1 rozwiązanie w task1_solution.txt

--- task1/task1.py
+++ task1/task1.py
@@ -2,8 +2,18 @@ import requests
 import xml.etree.ElementTree as ET
 
 
+
 def task1_solution():
-    pass
+
+    response = requests.get('https://coding-academy.pl/all_customers')
+    root = ET.fromstring(response.text)
+
+    for customer in root.findall('./customer'):
+        line_to_write = customer.text
+        #print(line_to_write)
+
+        with open("task1_solution.txt", "a") as f:
+            f.write(line_to_write + "\n")
 
 
 if __name__ == '__main__':
